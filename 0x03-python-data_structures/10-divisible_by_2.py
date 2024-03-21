8fba2b52e6a6482dab8b2d9abac0f9745eb79789
02teedd <magwanyathabng0@gmail>
2024-03-21 11:42:55 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) <= 0:
-        return None
-    else:
-        maximum = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > maximum:
-                maximum = my_list[i]
-        return maximum
+def divisible_by_2(my_list=[]):
+    length = len(my_list)
+    new_list = []
+    for i in range(length):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
