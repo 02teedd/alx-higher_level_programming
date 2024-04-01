dfeedc0bc3c57b51ca3b6597bfd7e02f4d3bc626
02teedd <magwanyathabng0@gmail>
2024-04-01 11:47:41 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in my_list:
-        print("{:d}".format(i))
+    if not matrix:
+    return []
+squared_matrix = [[0 for _ in row] for row in matrix]
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      if isinstance(matrix[i][j], int):
+        squared_matrix[i][j] = matrix[i][j] * matrix[i][j]
+      else:
+          squared_matrix[i][j] = matrix[i][j]
+
+  return squared_matrix
