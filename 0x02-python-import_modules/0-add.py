422ae3ee82f63fd7ae966b0f30be485a87f3a71e
02teedd <magwanyathabng0@gmail>
2024-04-01 08:51:11 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-if __name__ == "__main__":
-
-    a = 1
-    b = 2
-    sum = 1 + 2
-    print(f"{a} + {b} = {sum}")
-
-add(1, 2)
+a = 1
+b = 2
+from add_0 import add
+print(f"{} + {} = {}".format(a, b add(a, b)))
