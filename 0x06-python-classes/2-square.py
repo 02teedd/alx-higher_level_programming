489a3833456350b390cce4a0912bcd2c12959275
02teedd <magwanyathabng0@gmail>
2024-04-02 11:34:51 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,40 @@
 #!/usr/bin/python3
+
+"""
+this is simply our class square
+"""
+
+
 class Square:
+    """
+    we defining size
+    """
+
+
     def __init__(self, size=0):
         self.__size = size
+        """
+        this is our conditions
+        """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
+        """
+        property and inside we define self
+        """
         @property
         def size(self):
             return self.__size
-        
+        """
+        and size.setter
+        """
         @size.setter
         def size(self, value):
             self.__size = value
-            
+           """
+           lastly where we define area and return self.__size ** 2
+           """
             def area(self):
                 return self.__size ** 2
