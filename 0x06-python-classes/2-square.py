e89b2b84e43c62a38f045e69863f9d71afc71a2c
02teedd <magwanyathabng0@gmail>
2024-04-08 01:51:44 -0700

    files

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
+"""Defines a rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectanglr."""
+
+    def __init__(self, width=0, height=0):
+        """Initiatlize a new REctangle.
+
+        args:
+        width (int): The width of the new recttangle.
+        height (int): The height of the new rectangle.
         """
-        Initializes a Square instance.
+        self.width = width
+        self.height = height
 
-        Parameters:
-            size (init): Optional. The size of the sQuare.
+    @property
+    def width(self):
+        """Get/set the width of the Rectangle."""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinsrance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is negative.
-        """
+        @property
+        def height(self):
+            """Get/set the height of the Rectangle."""
+            return self.__height
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        @height.setter
+        def height(self, value):
