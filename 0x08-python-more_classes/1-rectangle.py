3790d5dedc16107266e7eaeea0958b8a97b538ad
02teedd <magwanyathabng0@gmail>
2024-04-11 01:34:28 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,11 +8,12 @@ class Rectangle:
         """initialize a new Rectangle.
 
         args:
-        width (int): The width of the new rectangle.
-        height (int): The height of the new rectangle.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
+
     @property
     def width(self):
         """Get/set the width of the rectangle."""
@@ -35,17 +36,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise valueError("height must be >= 0")
         self.__height = value
-
-    def __str__(self):
-        return f"{{'_Rectangle__height': {self.__height}, '_Rectangle__width': {self.__width}}}"
-
-
-rectangle1 = Rectangle(4, 2)
-rectangle2 = Rectangle(3, 10)
-
-"""print(rectangle1.__dict__)"""
-"""print(rectangle2.__dict__)"""
-
